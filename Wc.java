931bd7c384550475dcef9516fc0d81d3d0872f3d
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-02-19 19:59:45 +0530

    version 2 completed for happy paths gives the error the invalid options also

--- Wc.java
+++ Wc.java
@@ -3,67 +3,73 @@ import java.io.*;
 public class Wc{
 	private String str;
 	private String fileName;
+	private String options;
 
-	public Wc(String str, String fileName){
+	public Wc(String str, String fileName,String options){
 		this.str = str;
 		this.fileName = fileName;
+		this.options = options;
 	}
 
 	public int getStringLength(){
 		return str.length();
 	}
 		
-	public int giveNumberOfWords(){
-		String[] contantWords =  str.split("\\s+");
-		int count = 0;
-		for(int i = 0; i < contantWords.length; i++)
-			if(contantWords[i].equals(""))
-				count++;
-		return contantWords.length - count;
+	public int giveNumberOfWords() {
+		return str.split("[ \t\n]").length;
 	}
 
 	public int giveNumberOfLines(){
-		String[] contantChars = str.split("");
-		int count = 0;
-		for (int i = 0; i < contantChars.length; i++) {
-			if(contantChars[i].equals("\n"))
-				count++;
+		return str.split("\n").length;
+	}
+
+	public int giveIndex(){
+		String[] allOptions = {"-l","-w","-c"};
+		for (int i = 0;i < allOptions.length; i++) {
+			if(allOptions[i].substring(0,2).equals(options))
+				return i;
 		}
-		return count;
+		return -1;
 	}
 
 	public String toString(){
-		return "\t" + giveNumberOfLines() + "\t" + giveNumberOfWords() 
-		+ "\t" + getStringLength() + " " + fileName;
+		int[] contantFunction = {giveNumberOfLines(),giveNumberOfWords(),getStringLength()};
+		if(options == null){
+			return "\t" + giveNumberOfLines() + "\t" + giveNumberOfWords() 
+				+ "\t" + getStringLength() + " " + fileName;
+		}
+		if(giveIndex() == -1)
+			return "invalid option " + options.substring(1,2);
+		return "\t" + contantFunction[giveIndex()] + " " +fileName;
 	}
 }
 
 class WcJava{
 	public static String readFileContent(String file) throws Exception {
-		FileReader readFile = new FileReader(file); 
-		BufferedReader breakLines = new BufferedReader(readFile); 
-		String fileContent = ""; 
-		String readLines;
-		while((readLines = breakLines.readLine()) != null) { 
-			fileContent += readLines + "\r\n";
-		} 
-		readFile.close();
-		return fileContent;
+		try{	
+			FileReader readFile = new FileReader(file); 
+			BufferedReader breakLines = new BufferedReader(readFile); 
+			String fileContent = ""; 
+			String readLines;
+			while((readLines = breakLines.readLine()) != null) { 
+				fileContent += readLines + "\r\n";
+			} 
+			readFile.close();
+			return fileContent;
+		}catch(Exception e){
+			return null;
+		}
 	}
-
-	// public String[] allOption(String args[]){
-	// 	String[] options = new String[args.length];
-	// 	int count = 0;
-	// 	for (int i = 0;i < args.length; i++) {
-	// 		if(args[i].substring(0,1).equals("-"))
-	// 			options[count++] = args[i];
-	// 	}
-	// 	return options;
-	// }
 
 	public static void main(String args[]) throws Exception {
 		String fileContent = readFileContent(args[0]);
-		Wc w = new Wc(fileContent,args[0]);
-		System.out.println(w);
+		String options = "";
+		options = args.length == 1 ? null : args[1];
+		if(fileContent == null)
+			System.out.println("invalid file");
+		else{
+			Wc w = new Wc(fileContent,args[0],options);
+			System.out.println(w);
+		} 
 	}
 }
--- WcTest.java
+++ WcTest.java
@@ -6,32 +6,32 @@ public class WcTest{
 	String two = "my name is vijay pratap singh.\r\nwhat is your name?";
 	@Test
 	public void test_that_function_getStringLength_give_the_number_of_character_of_the_string(){
-		Wc w = new Wc(one,"one");
+		Wc w = new Wc(one,"one",null);
 		assertEquals(48, w.getStringLength());
 	}
 	@Test
 	public void test_that_function_getStringLength_give_the_number_of_character_of_the_string_two(){
-		Wc w = new Wc(two,"one");
+		Wc w = new Wc(two,"one",null);
 		assertEquals(50, w.getStringLength());
 	}
 	@Test
 	public void test_that_function_giveNumberOfWords_give_the_of_words_in_the_string(){
-		Wc w = new Wc(one,"one");
+		Wc w = new Wc(one,"one",null);
 		assertEquals(10, w.giveNumberOfWords());
 	}
 	@Test
 	public void test_that_function_giveNumberOfWords_give_the_of_words_in_the_string_for_two(){
-		Wc w = new Wc(two,"one");
+		Wc w = new Wc(two,"one",null);
 		assertEquals(10, w.giveNumberOfWords());
 	}
 	@Test
 	public void test_that_function_giveNumberOfLines_give_the_number_of_lines_in_the_string(){
-		Wc w = new Wc(one,"one");
+		Wc w = new Wc(one,"one",null);
 		assertEquals(2,w.giveNumberOfLines());	
 	}
 	@Test
 	public void test_that_function_giveNumberOfLines_give_the_number_of_lines_in_the_string_two(){
-		Wc w = new Wc(two,"one");
-		assertEquals(1,w.giveNumberOfLines());	
+		Wc w = new Wc(two,"one",null);
+		assertEquals(2,w.giveNumberOfLines());	
 	}
 }
